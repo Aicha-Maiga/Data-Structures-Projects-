a42417240858b7ae849746b2136469d389515f35
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-02 23:44:00 -0600

    Add files via upload

---
+++ ClockRepair.cs
@@ -0,0 +1,37 @@
+﻿// Written by Aicha Maiga
+// 2/2/2025
+
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace RepairingTheClockTower
+{
+    public class ClockRepair
+    {
+        //variables
+        private int number = -1;
+
+        // gets and sets
+        public int Number 
+        { 
+            get { return this.number; } 
+            set { this.number = value; }
+        }
+        // methods 
+
+        public string ClockSound()
+        {
+            string aSound = "n/a";
+
+            if (this.Number % 2 == 0)
+                aSound = "Tick";
+            else
+                aSound = "Tock";
+
+            return aSound;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,34 +1,19 @@
-﻿// Written by Aicha Maiga
+﻿// Wrtitten by Aicha Maiga
 // 2/2/2025
 
-namespace TheDefenseOfConsolas
+
+namespace RepairingTheClockTower
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            ClockRepair aClockRepair = new ClockRepair();
 
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Enter a number:");
+            aClockRepair.Number = Convert.ToInt32(Console.ReadLine());
 
-            Console.Title = "Defense of Consolas";
-
-
-            SquadDeployment aSquadDeployment = new SquadDeployment();
-
-
-            Console.WriteLine(" Target Row?");
-            aSquadDeployment.Row = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(" Target Column?");
-            aSquadDeployment.Column = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine($"({aSquadDeployment.Row}, {aSquadDeployment.Column - 1})");
-            Console.WriteLine($"({aSquadDeployment.Row -1}, {aSquadDeployment.Column})");
-            Console.WriteLine($"({aSquadDeployment.Row}, {aSquadDeployment.Column + 1})");
-            Console.WriteLine($"({aSquadDeployment.Row +1}, {aSquadDeployment.Column})");
-          
-            Console.Beep();
+            Console.WriteLine(aClockRepair.ClockSound());
         }
     }
 }
