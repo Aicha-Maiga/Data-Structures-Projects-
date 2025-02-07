83e7e711a89b96817fa8ae388db0521b8c22735a
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-06 23:19:34 -0600

    Add files via upload

---
+++ DiscountedInventory.Program.cs
@@ -0,0 +1,37 @@
+﻿// Written by Aicha Maiga
+
+using System.ComponentModel.Design;
+
+namespace P83DiscountedInventory
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DiscountedInventory aDiscountedInventory = new DiscountedInventory();
+
+            Console.WriteLine(aDiscountedInventory.ToString());
+
+            Console.WriteLine(" What number do you want to see the price of?");
+            aDiscountedInventory.Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your name:");
+            aDiscountedInventory.Name = Console.ReadLine() ?? string.Empty;
+
+            int actualprice = aDiscountedInventory.ItemPrice();
+            double discount = aDiscountedInventory.DiscountedPrice();
+
+            if (actualprice != discount) 
+            {
+                Console.WriteLine(aDiscountedInventory.DiscountedPrice());
+            }
+            else
+            {
+                Console.WriteLine(aDiscountedInventory.ItemPrice());
+            }
+                    
+
+            //Console.WriteLine(aDiscountedInventory.ItemPrice());
+        }
+    }
+}
---
+++ DiscountedInventory.cs
@@ -0,0 +1,91 @@
+﻿// Written by Aicha Maiga
+// 02/05/2025
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace P83DiscountedInventory
+{
+    public class DiscountedInventory
+    {
+        // variables 
+        private int number;
+        private string name ="n/a";
+
+        // gets and sets 
+        public int Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        // methods
+
+        public int ItemPrice()
+        {
+            int ItemPrice = 0;
+
+            switch (Number)
+            {
+                case 1:
+                    ItemPrice = 10;
+                    break;
+                case 2:
+                    ItemPrice = 16;
+                    break;
+                case 3:
+                    ItemPrice = 24;
+                    break;
+                case 4:
+                    ItemPrice = 2;
+                    break;
+                case 5:
+                    ItemPrice = 20;
+                    break;
+                case 6:
+                    ItemPrice = 200;
+                    break;
+                case 7:
+                    ItemPrice = 2;
+                    break;
+            }
+            return ItemPrice;
+        }
+
+        public double DiscountedPrice()
+        {
+            double Price = ItemPrice();
+
+            if ( Name == "Aicha")
+            {
+                Price *= 0.5;
+            }
+            return Price;
+        }
+
+        public override string ToString()
+        {
+            string message = "";
+            message = message + " The following items are available:" + "\n";
+            message = message + " 1- Rope" + "\n";
+            message = message + " 2- Torches" + "\n";
+            message = message + " 3-Climbing Equipment" + "\n";
+            message = message + " 4- Clean Water" + "\n";
+            message = message + " 5- Machete" + "\n";
+            message = message + " 6- Canoe" + "\n";
+            message = message + " 7- Food Supplies" + "\n";
+
+            return message;
+        }
+
+
+    }
+}
+    
+
