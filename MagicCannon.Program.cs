eb56feb700e8fe6098b8e51beeb2e1107d289c33
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-06 23:20:45 -0600

    Add files via upload

---
+++ MagicCannon.Program.cs
@@ -0,0 +1,41 @@
+﻿// Written by Aicha Maiga
+// 02/05/2025
+
+namespace p89TheMagicCannon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TheMagicCannon aMagicCannon = new TheMagicCannon();
+
+            for (int number = 1; number <= 100; number++)
+            {
+                string blast = aMagicCannon.CannonBlast(number);
+
+                if (blast == "Electric and Fire")
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if (blast == "Electric")
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else if (blast == "Fire")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine($"{number}: {blast}");
+                Console.ResetColor();
+
+            }
+
+        }
+        
+    }
+}
---
+++ TheMagicCannon.cs
@@ -0,0 +1,55 @@
+﻿// Written by Aicha Maiga
+// 02/05/2025
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+
+namespace p89TheMagicCannon
+{
+    public class TheMagicCannon
+    {
+        // variables
+        private int number = -1;
+        
+        //gets and sets 
+        public int Number 
+        { 
+            get { return this.number; } 
+            set { this.number = value; } 
+        }
+
+        // methods
+        public string CannonBlast(int number )
+        {
+            
+            //for (int number = 1; number <= 100; number++)
+            {
+
+
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    return "Electric and Fire";
+                }
+                else if (number % 5 == 0)
+                {
+                    return "Electric";
+                }
+                else if (number % 3 == 0)
+                {
+                    return "Fire";
+                }
+                else
+                {
+                    return "Normal";
+
+                }
+            }
+ 
+        }
+
+        }
+    }
+
