78b18b3a16e8c6629317cb7afa4abd8efdf5904b
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-06 23:20:08 -0600

    Add files via upload

---
+++ ThePrototype.Program.cs
@@ -0,0 +1,38 @@
+﻿// Written by Aicha Maiga
+//02/05/2025
+
+namespace ThePrototype
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ThePrototype _ThePrototype = new ThePrototype();
+            int number;
+
+            Console.WriteLine("User 1, please enter a number between 0 and 100:");
+            number = Convert.ToInt32(Console.ReadLine());
+           
+            while (number < 0 || number > 100);
+
+            _ThePrototype.NumberUser1 = number;  
+
+            Console.Clear();
+            Console.WriteLine("User 2, guess the number.");
+
+            while (true)
+            {
+                Console.Write("What is your next guess? ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                string result = _ThePrototype.EvaluateGuess(guess); 
+                Console.WriteLine(result);
+
+                if (result == "You guessed the number!")
+                    break;
+            }
+        }
+    }
+
+}
+
---
+++ ThePrototype.cs
@@ -0,0 +1,35 @@
+﻿// Written by Aicha Maiga
+// 02/05/2025
+
+using System;
+using System.Collections.Generic;
+
+
+namespace ThePrototype
+{
+
+    public class ThePrototype
+    {
+        // variables
+        private int numberUser1 = -1;
+
+        // gets and sets
+
+        public int NumberUser1
+        {
+            get { return this.numberUser1; }
+            set { this.numberUser1 = value; }
+        }
+
+        // methods 
+        public string EvaluateGuess(int guess)
+        {
+            if (guess > NumberUser1)
+                return $"{guess} is too high.";
+            else if (guess < NumberUser1)
+                return $"{guess} is too low.";
+            else
+                return "You guessed the number!";
+        }
+    }
+}
