bb2a4ba327fdcc6e3cc1f5c54df38d68cda2515f
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-16 20:31:35 -0600

    Add files via upload

---
+++ SimulaTest.cs
@@ -0,0 +1,62 @@
+﻿// Written by Aicha Maiga 
+// 02/14/2025
+
+
+using System;
+using System.Collections.Generic;
+
+
+namespace P135Simula_sTest
+{
+    public class SimulaTest
+    {
+        private StateOfChest currentState;
+        public enum StateOfChest { Open, Closed, Locked }
+
+       
+        public string Closing()
+
+        {
+            //string message = "";
+
+            if (currentState == StateOfChest.Open)
+            {
+                currentState = StateOfChest.Closed;
+            }
+            return "The chest is closed " ;
+        }
+        public string Locking()
+
+        {
+
+            if (currentState == StateOfChest.Closed)
+            {
+                currentState = StateOfChest.Locked;
+                
+            }
+            return "The chest is locked"; 
+        }
+        public string Unlocking()
+
+        {
+
+            if (currentState == StateOfChest.Locked)
+            {
+                currentState = StateOfChest.Closed;
+            }
+            return "The chest is unlocked " ;
+        }
+        public string Opening()
+
+        {
+
+            if (currentState == StateOfChest.Closed)
+            {
+                currentState = StateOfChest.Open;
+            }
+            return "The chest is open" ;
+        }
+        
+    }
+}
+
---
+++ SimulaTestProgram.cs
@@ -0,0 +1,45 @@
+﻿// Written by Aicha Maiga
+//02/14/2025
+
+using System.Threading.Channels;
+
+namespace P135Simula_sTest
+{
+    internal class SimulaTestProgram
+    {
+        public static void Main(string[] args)
+        {
+            
+            SimulaTest aSimulaTest = new SimulaTest();
+            Console.WriteLine("The current state is locked");
+
+            while (true)
+            {
+                
+                Console.WriteLine("What do you want to do?");
+                //string input = Console.ReadLine();
+                string input = Console.ReadLine()?.Trim().ToLower() ?? "";
+
+
+                if (input == "open")
+                {
+                    Console.WriteLine(aSimulaTest.Opening());
+                }
+                else if (input == "close")
+                {
+                    Console.WriteLine(aSimulaTest.Closing());
+                }
+                else if (input == "lock")
+                {
+                    Console.WriteLine(aSimulaTest.Locking());
+                }
+                else if (input == "unlock")
+                {
+                    Console.WriteLine(aSimulaTest.Unlocking());
+                }
+
+               
+            }
+        }
+    }
+}
