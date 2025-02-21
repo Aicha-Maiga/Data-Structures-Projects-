9fa28ccaf50974e2b4ac74dfdf88e5c7adcc4dcc
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-20 18:41:59 -0600

    Add files via upload

---
+++ FirstLastChar.cs
@@ -0,0 +1,36 @@
+﻿// Written by Aicha Maiga 
+// 02/19/2025
+
+
+using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace HW4FirstandLast
+{
+    public class FirstLastChar
+    {
+        public string FindFirstLastChar(string sentence )
+        {
+            string result = "";
+
+            if (sentence.Length < 2)
+            {
+                return "Incompatible";
+            }
+            else if (sentence[0] == sentence[sentence.Length - 1])
+            {
+                return "Two's a pair.";
+            }
+            else
+            {
+                char firstletter = sentence[0];
+                char lastletter= sentence[sentence.Length - 1];
+                result = lastletter + sentence.Substring(1, sentence.Length - 2) + firstletter;
+            }
+            return result; 
+
+        }
+     }
+}
---
+++ FirstLastCharProgram.cs
@@ -0,0 +1,15 @@
+﻿// Written by Aicha Maiga 
+// 02/19/2025
+
+
+namespace HW4FirstandLast
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FirstLastChar  theFirstLastChar = new FirstLastChar();
+            Console.WriteLine(theFirstLastChar.FindFirstLastChar("Cat, dog, and mouse."));
+        }
+    }
+}
