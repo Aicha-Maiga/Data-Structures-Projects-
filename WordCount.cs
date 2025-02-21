e9e7c364639b74b1a4c74b1b3376329116f70406
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-20 18:40:14 -0600

    Add files via upload

---
+++ WordCount.cs
@@ -0,0 +1,20 @@
+﻿// Written by Aicha Maiga 
+// 02/19/2025
+
+using System;
+using System.Collections.Generic;
+
+
+namespace HW4WordCount
+{
+    public class WordCount
+    {
+        public int FindWordCount(string sentence)
+        {
+            string [] words = sentence.Split(' ');
+
+            int wordcount = words.Length;
+            return wordcount;
+        }
+    }
+}
---
+++ WordCountProgram.cs
@@ -0,0 +1,17 @@
+﻿// Written by Aicha Maiga 
+// 02/19/2025
+
+
+namespace HW4WordCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WordCount theWordCount = new WordCount();
+
+            Console.WriteLine(theWordCount.FindWordCount("This is a test"));
+            Console.WriteLine(theWordCount.FindWordCount("What an easy task, right"));
+        }
+    }
+}
