758e12c6ad6328c496ee08f49c4371c24ebab184
Aicha Maiga <156473208+Aicha-Maiga@users.noreply.github.com>
2025-02-20 18:43:31 -0600

    Add files via upload

---
+++ LettersInOrder.cs
@@ -0,0 +1,25 @@
+﻿// Written by Aicha Maiga 
+// 02/19/2025
+
+
+using System;
+using System.Collections.Generic;
+
+namespace HW4IsInOrderorNot
+{
+    public class LettersInOrder
+    {
+        public bool ISinOrder(string word)
+        {
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] < word[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LettersinOrderProgram.cs
@@ -0,0 +1,18 @@
+﻿// Written by Aicha Maiga 
+// 02/19/2025
+
+namespace HW4IsInOrderorNot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LettersInOrder  theLettersInOrder = new LettersInOrder();
+
+            Console.WriteLine(theLettersInOrder.ISinOrder("abc"));
+            Console.WriteLine(theLettersInOrder.ISinOrder("edabit"));
+            Console.WriteLine(theLettersInOrder.ISinOrder("123"));
+
+        }
+    }
+}
